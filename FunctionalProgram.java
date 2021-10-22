8d3018a599c67433c77d09accedc8186ce622991
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-22 14:45:15 +0530

    ADDED: Welcome Message

---
+++ FunctionalProgram.java
@@ -0,0 +1,10 @@
+package com.functional.program;
+
+public class FunctionalProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Functional Computation Program");
+	}
+
+}
